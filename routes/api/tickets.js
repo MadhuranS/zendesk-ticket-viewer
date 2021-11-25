b9b142e0a661862fd0997a12f30765078e2eb352
Madhu Sivapragasam <madhuran.siva@gmail.com>
2021-11-25 02:54:54 -0500

    creating route to get all tickets

--- routes/api/tickets.js
+++ routes/api/tickets.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const config = require("config");
+const axios = require("axios");
 
 // @route GET api/tickets
-// @desc  Test route
-router.get("/", (req, res) => res.send("Tickets route"));
+// @desc  Get all tickets from zendesk
+router.get("/", async (req, res) => {
+    const axiosConfig = {
+        headers: {
+            Authorization: `${config.get(`zendeskSecret`)}`,
+        },
+    };
+    try {
+        const body = await axios.get(
+            "https://zccmadhu.zendesk.com/api/v2/tickets.json",
+            axiosConfig
+        );
+
+        res.json(body.data);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Server Error");
+    }
+});
 
 module.exports = router;
