a450a4c85ede3dcaebbcb079ee472163d699cafa
Madhu Sivapragasam <madhuran.siva@gmail.com>
2021-11-25 03:56:17 -0500

    setting up redux rootreducer and store

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+import Tickets from "./components/tickets/Tickets";
+//imports for Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./App.css";
 
 const App = () => (
-    <Fragment>
-        <h1>app</h1>
-    </Fragment>
+    <Provider store ={store}>
+        <Router>
+            <Fragment>
+                <Tickets></Tickets>
+                <Routes></Routes>
+            </Fragment>
+        </Router>
+    </Provider>
 );
 
 export default App;
