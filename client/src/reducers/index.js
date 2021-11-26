933f221595c10d84acec982b2a4c4d3f2e210328
Madhu Sivapragasam <madhuran.siva@gmail.com>
2021-11-26 14:35:47 -0500

    code formatting changes

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import alert from "./alert";
-import tickets from "./tickets"
+import tickets from "./tickets";
 
 export default combineReducers({
     alert,
