b2e5501d6d3371244905e9883e6d22ca734d66d6
Madhu Sivapragasam <madhuran.siva@gmail.com>
2021-11-29 03:20:32 -0500

    make port 5000 default port

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = express();
 //Define Routes
 app.use("/api/tickets", require("./routes/routing/tickets"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
